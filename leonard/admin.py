d8d14f4ef22d3331560781cd44e8386226d96a6d
Leonard Moses <qalexeon@Xavier.lan>
2022-02-27 01:11:41 -0500

    created model of Art Pieces

--- leonard/admin.py
+++ leonard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ArtPiece
+
+admin.site.register(ArtPiece)
--- leonard/models.py
+++ leonard/models.py
@@ -1,3 +1,15 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+# leonard/models.py
+class ArtPiece(models.Model):
+    title = models.CharField(max_length=100)
+    decription = models.CharField(max_length=500)
+    photo_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    
